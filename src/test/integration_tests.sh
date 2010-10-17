91dc5e7e5321bae08fdf864bf6163f288f8987f6
Miro Bezjak <bezjak.miro@gmail.com>
2010-10-17 21:05:16 +0200

    Add resolve_napalm_home function.
    NAPALM_HOME will be set only if it hasn't be set already.

--- src/test/integration_tests.sh
+++ src/test/integration_tests.sh
@@ -18,5 +18,15 @@ testIllegalArgument() {
   assertEquals 10 $?
 }
 
+testNapalmHomeCanBeAssigned() {
+  local _tmp=`mktemp -d`
+  cp -pR ../dist $_tmp
+
+  local _home=`export NAPALM_HOME=$_tmp && $NAPALM -v 2>&1 | awk '/^NAPALM_HOME/{print $3}'`
+  rm -rf $_tmp
+
+  assertEquals $_tmp $_home
+}
+
 
 . ./shunit2
