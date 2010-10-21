2b5650d1b9cf1886b5f57ee345a4ef03282ebf47
Miro Bezjak <bezjak.miro@gmail.com>
2010-10-21 17:35:35 +0200

    Small refactoring.
    Some code is now in fixture blok instead being in expectation
    block.

--- src/test/unit_test_list_plugins.sh
+++ src/test/unit_test_list_plugins.sh
@@ -8,13 +8,13 @@ testListPlugins() {
   local tmp=`mktemp -d`
   mkdir -p $tmp/global/plugins
   mkdir -p $tmp/local/plugins
+  touch $tmp/global/plugins/a;
+  touch $tmp/global/plugins/b;
+  touch $tmp/local/plugins/c;
+  touch $tmp/local/plugins/d;
 
   (NAPALM_HOME=$tmp/global;
    NAPALM_USER_HOME=$tmp/local;
-   touch ${NAPALM_HOME}/plugins/a;
-   touch ${NAPALM_HOME}/plugins/b;
-   touch ${NAPALM_USER_HOME}/plugins/c;
-   touch ${NAPALM_USER_HOME}/plugins/d;
    assertEquals 'Plugins[4]: a b c d' "`list_plugins`")
 
   rm -rf $tmp
