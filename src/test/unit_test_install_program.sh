4c8821e92e0f7c953283f8ea3d9a555e39128d0a
Miro Bezjak <bezjak.miro@gmail.com>
2011-02-02 23:23:55 +0100

    Implement install_program function.
    install_program implements installation of programs via plugins.

--- src/test/unit_test_install_program.sh
+++ src/test/unit_test_install_program.sh
@@ -45,14 +45,98 @@ EOF
    assertEquals true $plugin_was_executed)
 }
 
+testCreateAndChangeToTempDirectory() {
+  (mktemp() {
+    assertEquals '-d' $1
+    assertEquals '/tmp/napalm.XXXXXXXXX' $2
+    echo $pd
+   }; create_and_change_to_temp_directory;
+   assertEquals $PWD $pd)
+}
+
+testDownloadProgramWgetFailsToDownloadAnything() {
+  (wget() {
+    return 1
+   }; download_program 'http://foo.com http://bar.com';
+   assertEquals 1 $?)
+}
+
+testDownloadProgramWgetSucceedsOnSecondTry() {
+  (local try=0;
+   wget() {
+    let try="$try + 1"
+    [[ $try -eq 1 ]] && return 1 || return 0
+   }; download_program 'http://foo.com http://bar.com';
+   assertEquals 2 $try
+   assertEquals 0 $?)
+}
+
+testDownloadProgramWgetSucceedsOnFirstTry() {
+  (wget() {
+    return 0
+   }; download_program 'http://foo.com http://bar.com';
+   assertEquals 0 $?)
+}
+
+testExtractProgramUnknownExtension() {
+  (cd $pd;
+   extract_program 'example.7z' extracted;
+   assertEquals 1 $?;
+   assertTrue "[ -d extracted ]")
+}
+
+testExtractProgramExtensionTarGz() {
+  (cd $pd;
+   tar() {
+    assertEquals "--gzip" $1
+    assertEquals "--directory" $2
+    assertEquals "extracted" $3
+    assertEquals "-xf" $4
+    assertEquals "example.tar.gz" $5
+    return 0
+   };
+   extract_program 'example.tar.gz' extracted;
+   assertEquals 0 $?;
+   assertTrue "[ -d extracted ]")
+}
+
+testExtractProgramExtensionTarBz2() {
+  (cd $pd;
+   tar() {
+    assertEquals "--bzip2" $1
+    assertEquals "--directory" $2
+    assertEquals "extracted" $3
+    assertEquals "-xf" $4
+    assertEquals "example.tar.bz2" $5
+    return 0
+   };
+   extract_program 'example.tar.bz2' extracted;
+   assertEquals 0 $?;
+   assertTrue "[ -d extracted ]")
+}
+
+testExtractProgramExtensionZip() {
+  (cd $pd;
+   unzip() {
+    assertEquals "-q" $1
+    assertEquals "example.zip" $2
+    assertEquals "-d" $3
+    assertEquals "extracted" $4
+    return 0
+   };
+   extract_program 'example.zip' extracted;
+   assertEquals 0 $?;
+   assertTrue "[ -d extracted ]")
+}
+
 testInstallProgramNameEmpty() {
   install_program
-  assertEquals 2 $?
+  assertEquals 4 $?
 }
 
 testInstallProgramVersionEmpty() {
   install_program foo
-  assertEquals 3 $?
+  assertEquals 5 $?
 }
 
 testInstallProgramPluginNotLoaded() {
