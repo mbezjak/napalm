7cb19b7dfd8a85d4b2a725d34f03ebbfbe7c259e
Miro Bezjak <bezjak.miro@gmail.com>
2014-05-28 13:54:44 +0200

    release.sh: use release-* commands from poly-devel to DRY

--- release.sh
+++ release.sh
@@ -5,24 +5,7 @@
 set -o errexit
 
 declare -r version="$1"
-
-function validate-changelog {
-    grep -q "^# $version" Changelog.md ||
-        fail "Missing changelog entry for version: $version"
-}
-
-validate-version-set "$version"
-validate-uncommited
-validate-not-tagged "$version"
-validate-new-version "$version"
-validate-changelog
-
-update-version "$version"
-validate-version-match "$version"
-
-repo-commit-all "RELEASE $version"
-git tag --annotate "$version" --message="Tag for release $version"
-git push
-git push --tags
+release-pre "$version"
+release-post "$version"
 
 exit 0
